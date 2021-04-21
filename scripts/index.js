1e21409a56ee6d34420167d5dbc310cd2ba85a93
Darya Churukina <DaryaChurukina@yandex.ru>
2021-04-21 17:05:41 +0400

    feat: edit button

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-let editButton = document.querySelectorAll(".profile__edit-buttond");
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let popupClose = document.querySelector(".popup__close");
+
+function togglPopup() {
+  let isOpen = popup.classList.contains("popup_visible");
+
+  if (isOpen) {
+    popup.classList.remove("popup_visible");
+  } else {
+    popup.classList.add("popup_visible");
+  }
+}
+
+editButton.addEventListener("click", togglPopup);
+popupClose.addEventListener("click", togglPopup);
