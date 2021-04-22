ddbba629846730c4ef36297b44cdd5c017d6a000
Darya Churukina <DaryaChurukina@yandex.ru>
2021-04-22 15:20:56 +0400

    feat: edit button

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,32 @@
-let editButton = document.querySelector(".profile__edit-button");
+let profileName = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__subtitle");
+let profileEditButton = document.querySelector(".profile__edit-button");
+
 let popup = document.querySelector(".popup");
-let popupClose = document.querySelector(".popup__close");
+let popupCloseButton = document.querySelector(".popup__close");
+let popupForm = document.querySelector(".popup__form");
+let popupNameInput = document.querySelector(".popup__input[name=name]");
+let popupDescriptionInput = document.querySelector(
+  ".popup__input[name=description]"
+);
 
-function togglPopup() {
-  let isOpen = popup.classList.contains("popup_visible");
+profileEditButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", closePopup);
+popupForm.addEventListener("submit", submitPopup);
 
-  if (isOpen) {
-    popup.classList.remove("popup_visible");
-  } else {
-    popup.classList.add("popup_visible");
-  }
+function submitPopup(event) {
+  event.preventDefault();
+  profileName.textContent = popupNameInput.value;
+  profileDescription.textContent = popupDescriptionInput.value;
+  closePopup();
 }
 
-editButton.addEventListener("click", togglPopup);
-popupClose.addEventListener("click", togglPopup);
+function openPopup() {
+  popupNameInput.value = profileName.textContent;
+  popupDescriptionInput.value = profileDescription.textContent;
+  popup.classList.add("popup_visible");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_visible");
+}
